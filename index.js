2e3c50f0dc697ee839f25e64d795bf2e4c85948b
Ahmed Essam <webdev.ahmed.essam255@gmail.com>
2026-08-30 19:18:17 +0300

    General CORS allowance

--- index.js
+++ index.js
@@ -16,16 +16,7 @@ const app = express();
 app.use(morgan('dev'));
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: 'http://localhost:3000',
-    credentials: true,
-  },
-  {
-    origin: 'https://sheikh-eraki.vercel.app',
-    credentials: true,
-  }),
-);
+app.use(cors());
 
 app.use('/users', users);
 app.use('/admin', admin);
